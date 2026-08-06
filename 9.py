acefed7199ca9e3834e5207c37eb9aba5b889815
kbarriosr21-star <kbarriosr21@campusuninunez.edu.co>
2026-08-06 15:44:26 -0500

    Update 9.py

---
+++ 9.py
@@ -0,0 +1,26 @@
+nota = int(input("Ingrese una nota entre 0 y 10: "))
+
+if nota == 0:
+    print("cero")
+elif nota == 1:
+    print("uno")
+elif nota == 2:
+    print("dos")
+elif nota == 3:
+    print("tres")
+elif nota == 4:
+    print("cuatro")
+elif nota == 5:
+    print("cinco")
+elif nota == 6:
+    print("seis")
+elif nota == 7:
+    print("siete")
+elif nota == 8:
+    print("ocho")
+elif nota == 9:
+    print("nueve")
+elif nota == 10:
+    print("diez")
+else:
+    print("La nota no es válida")

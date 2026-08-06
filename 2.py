9a89b3603a0b92e4b4d43a1aad60eb4019799f95
kbarriosr21-star <kbarriosr21@campusuninunez.edu.co>
2026-08-06 15:42:46 -0500

    Update 2.py

---
+++ 2.py
@@ -0,0 +1,8 @@
+n1 = int(input ("ingrese el primer numero: "))
+n2 = int(input ( "ingrese el segundo numero: "))
+
+if n1 == n2:
+    print("los numeros son iguales")
+
+else:
+    print("los numeros no son iguales")

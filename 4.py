a73765fc5f7a73cb86ccf93765a95f5ba216bd57
kbarriosr21-star <kbarriosr21@campusuninunez.edu.co>
2026-08-06 15:43:24 -0500

    Update 4.py

---
+++ 4.py
@@ -0,0 +1,11 @@
+n1 = int(input("ingrese el primer numero: "))
+n2 = int(input("ingrese el segundo numero: "))
+
+if n1 > n2:
+    print("el primer umero es mayor que el segundio")
+    
+elif n1 < n2:
+    print("el primer numero es menor que el segundo")
+
+else:
+    print("los numeros son iguales ")

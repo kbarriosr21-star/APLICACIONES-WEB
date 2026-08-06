66ae7aa94bc1a50f84889491b78694c1a5fb92f7
kbarriosr21-star <kbarriosr21@campusuninunez.edu.co>
2026-08-06 15:43:06 -0500

    Update 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+n1 = int(input("ingrese el primer numero: "))
+n2 = int(input("ingrese el segundo numero:" ))
+
+if n1 % n2 == 0:
+    print("el primer numero es dicvisible por el segundo")
+else:
+    print("el segundo numero n oes divisible por el segundo")
